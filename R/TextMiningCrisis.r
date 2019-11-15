07f498d0e418c54222269a88411ff6fe9c910203
Manuel.Betin <manuel.betin@oecd.org>
2019-11-15 10:16:23 +0100

    include radar charts for reforms and for items of the structure of the economy

--- R/TextMiningCrisis.r
+++ R/TextMiningCrisis.r
@@ -1909,11 +1909,14 @@ country_radar_dt=function(table_N_occurence,isoc,top_n=50,weight_method="brut_fr
 
 country_radar_fig=function(country_radar_dt){
   
-  endo_exo_order=c("Wars","Natural_disaster","Commodity_crisis","World_outcomes",
+  endo_exo_order=c("Wars","Natural_disaster","Commodity_crisis","trade_crisis","World_outcomes",
                    "Expectations","Contagion","Balance_payment_crisis","Reduction_reserves",
                    "Currency_crisis","Banking_crisis","Financial_crisis","Severe_recession","Soft_recession",
-                   "Inflation_crisis","Political_crisis","Fiscal_outcomes","Fiscal_consolidation","Sovereign_default")
+                   "Inflation_crisis","Political_crisis","Social_crisis","Fiscal_outcomes","Fiscal_consolidation","Sovereign_default")
   
+  reforms=c("Deregulation","Reform_agenda","Trade_reforms","Financial_reforms","Labor_market_reforms","Tax_reforms","Banking_reforms")
+  
+  structure=c("Concessional_lending","Short_term_debt","floating_rate_debt","foreign_debt","Fixed_exchange_rate","Floating_exchange_rate","Technical_assistance")
   
   country_radar_dt=country_radar_dt %>%
     dplyr::arrange(match(Crisis,endo_exo_order)) %>% dplyr::filter(Crisis %in% endo_exo_order)
@@ -1969,6 +1972,125 @@ country_radar_fig=function(country_radar_dt){
     font = list(family = "serif", size = 20),
     legend = list(bgcolor = "transparent",title="test"),
     showlegend = FALSE
+    )
+  p
+  
+}
+
+radar_reforms_fig=function(country_radar_dt){
+  
+  # endo_exo_order=c("Wars","Natural_disaster","Commodity_crisis","trade_crisis","World_outcomes",
+  #                  "Expectations","Contagion","Balance_payment_crisis","Reduction_reserves",
+  #                  "Currency_crisis","Banking_crisis","Financial_crisis","Severe_recession","Soft_recession",
+  #                  "Inflation_crisis","Political_crisis","Social_crisis","Fiscal_outcomes","Fiscal_consolidation","Sovereign_default")
+  # 
+  endo_exo_order=c("Deregulation","Reform_agenda","Trade_reforms","Financial_reforms","Labor_market_reforms","Tax_reforms","Banking_reforms")
+  # 
+  # structure=c("Concessional_lending","Short_term_debt","floating_rate_debt","foreign_debt","Fixed_exchange_rate","Floating_exchange_rate","Technical_assistance")
+  # 
+  country_radar_dt=country_radar_dt %>%
+    dplyr::arrange(match(Crisis,endo_exo_order)) %>% dplyr::filter(Crisis %in% endo_exo_order)
+  
+  Weights=country_radar_dt[,"word_weight"]$word_weight*10000
+  
+  Weights[Weights==0]=0.0000001
+  Crisis=stringr::str_replace_all(country_radar_dt[,"Crisis"]$Crisis,"_"," ")
+  
+  i=6
+  j=6 
+  s=5
+  alpha_endo=0.8
+  p <- plotly::plot_ly(type = 'scatterpolar',
+                       mode="lines") %>%
+    plotly::add_trace(
+      r = rep(2.5,length(Crisis)), #max(Weights)
+      theta = Crisis,
+      line=list(color="lightgrey")) %>%
+    # add_trace(
+    #   r = c(rep(0,i),rep(max(Weights,na.rm=T),j),rep(0,length(Crisis)-j-i)),
+    #   theta = Crisis,
+    #   name="Exogenous",
+    #   line=list(color=toRGB("orange",alpha_endo)),
+    #   fill = 'toself',fillcolor = toRGB("orange",alpha_endo)) %>%
+    # add_trace(
+    #   r = c(rep(max(Weights,na.rm=T),i),rep(0,length(Crisis)-i)),
+    #   theta = Crisis,
+    #   name="Pure Exogenous",
+    #   line=list(color=toRGB("green",alpha_endo)),
+  #   fill = 'toself',fillcolor = toRGB("green",alpha_endo)) %>%
+  # add_trace(
+  #   r = c(rep(0,length(Crisis)-s),rep(max(Weights,na.rm=T),s)),
+  #   theta = Crisis,
+  #   name="Endogenous",
+  #   line=list(color=toRGB("red",alpha_endo)),
+  #   fill = 'toself',fillcolor = toRGB("red",alpha_endo)) %>%
+  plotly::add_trace(
+    r = Weights,
+    theta = Crisis,
+    name="Profile of crisis",
+    line=list(color="#709Bff"),
+    fill = 'toself',fillcolor = '#709Bff',alpha_endo) %>%
+    plotly::layout(xaxis = 3, yaxis = median(Weights,na.rm=T)) %>%
+    
+    plotly::layout(
+      xaxis = list(title = "", showgrid = T, zeroline = F, showticklabels = T,
+                   domain = c(0, 3)
+      ),
+      yaxis = list(title = country_radar_dt$ISO3_Code %>% unique(), showgrid = F, zeroline = F, showticklabels = F#,
+                   #domain = c(0, 0.92)
+      ),
+      font = list(family = "serif", size = 20),
+      legend = list(bgcolor = "transparent",title="test"),
+      showlegend = FALSE
+    )
+  p
+  
+}
+
+radar_structure_fig=function(country_radar_dt){
+  
+  # endo_exo_order=c("Wars","Natural_disaster","Commodity_crisis","trade_crisis","World_outcomes",
+  #                  "Expectations","Contagion","Balance_payment_crisis","Reduction_reserves",
+  #                  "Currency_crisis","Banking_crisis","Financial_crisis","Severe_recession","Soft_recession",
+  #                  "Inflation_crisis","Political_crisis","Social_crisis","Fiscal_outcomes","Fiscal_consolidation","Sovereign_default")
+  # 
+  #endo_exo_order=c("Deregulation","Reform_agenda","Trade_reforms","Financial_reforms","Labor_market_reforms","Tax_reforms","Banking_reforms")
+  # 
+  endo_exo_order=c("Concessional_lending","Short_term_debt","floating_rate_debt","foreign_debt","Fixed_exchange_rate","Floating_exchange_rate","Technical_assistance")
+  # 
+  country_radar_dt=country_radar_dt %>%
+    dplyr::arrange(match(Crisis,endo_exo_order)) %>% dplyr::filter(Crisis %in% endo_exo_order)
+  
+  Weights=country_radar_dt[,"word_weight"]$word_weight*10000
+  
+  Weights[Weights==0]=0.0000001
+  Crisis=stringr::str_replace_all(country_radar_dt[,"Crisis"]$Crisis,"_"," ")
+  
+  alpha_endo=0.8
+  p <- plotly::plot_ly(type = 'scatterpolar',
+                       mode="lines") %>%
+    plotly::add_trace(
+      r = rep(2.5,length(Crisis)), #max(Weights)
+      theta = Crisis,
+      line=list(color="lightgrey")) %>%
+  plotly::add_trace(
+    r = Weights,
+    theta = Crisis,
+    name="Profile of crisis",
+    line=list(color="#709Bff"),
+    fill = 'toself',fillcolor = '#709Bff',alpha_endo) %>%
+    plotly::layout(xaxis = 3, yaxis = median(Weights,na.rm=T)) %>%
+    
+    plotly::layout(
+      xaxis = list(title = "", showgrid = T, zeroline = F, showticklabels = T,
+                   domain = c(0, 3)
+      ),
+      yaxis = list(title = country_radar_dt$ISO3_Code %>% unique(), showgrid = F, zeroline = F, showticklabels = F#,
+                   #domain = c(0, 0.92)
+      ),
+      font = list(family = "serif", size = 20),
+      legend = list(bgcolor = "transparent",title="test"),
+      showlegend = FALSE
     )
   p
   
