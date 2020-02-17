f926aaae641487e362182520cbd037d663ada592
Umberto Collodel <umberto.collodel@gmail.com>
2020-02-17 11:38:00 +0100

    major change: weset equal to NA problematic documents - string detection - (see Overleaf for more extented expl.) in the tf

--- R/tf.r
+++ R/tf.r
@@ -11,13 +11,28 @@ tf = function(corpus, keywords, brute_freq = F, parrallel = T) {
     
     if (is.vector(keywords) & !is.null(keywords)) {
         table = lapply(corpus, function(x) {
-            output = try(sum(eval_pages(x, keywords, brute_freq = brute_freq, 
+          
+          # Exclude minutes meetings, press releases and working papers erroneously scraped.
+          # Check the first page of each document for bad keyword.
+          
+          valid_document <- tibble(first_page = tolower(x[[1]])) %>% 
+            filter(!str_detect(first_page, paste(key_words_crisis()[["Problematic_documents"]], collapse = "|"))) %>% 
+            nrow()
+          
+          # If is a valid document, continue with tf-calculation. Else, return NA.
+          
+          if (valid_document == 1){
+                output = try(sum(eval_pages(x, keywords, brute_freq = brute_freq, 
                 parrallel = parrallel)[, 1]))
             if ("try-error" %in% class(output)) {
                 cat(crayon::red(paste0("Warning: error when mining ", keywords)))
                 output = 0
             }
             output
+          }
+        else { 
+          NA
+        }
         })
         names(table) = names(corpus)
         table = do.call(rbind, table)
@@ -29,3 +44,6 @@ tf = function(corpus, keywords, brute_freq = F, parrallel = T) {
         cat(crayon::red("please provide a vector of strings as argument for keywords"))
     }
 }
+
+
+
