b65c84e102f71ebe832f3143d9915d8916fe0846
manuelbetin <57252006+manuelbetin@users.noreply.github.com>
2020-03-10 13:07:59 +0100

    update comment in roxygene style

--- R/binary_freq_trans.r
+++ R/binary_freq_trans.r
@@ -1,6 +1,16 @@
 # functions to transform brut frequency into alternative metrics
 binary_freq_trans = function(table_N_occurence) {
-    # transform table from Number of occurence to binary variables
+    #' binary transformation of tfidf
+    
+    #' transform table from Number of occurence to binary variables
+    
+    #' @param table_N_occurence a dataframe with numerical columns corresponding
+    #' to the tf idf of each category
+    #' @author Manuel Betin
+    #' @return a dataframe with binary frequencies 0 if the tf_idf is zero and 1
+    #' if it is non zero
+    #' @export
+    
     binary_trans = function(x) {
         ifelse(x > 0, 1, 0)
     }
